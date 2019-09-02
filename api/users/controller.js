5a0beebb96975c2f17d2534abd81522a573ffc17
hi-captain <7273captain@gmail.com>
2019-09-02 10:43:12 +0900

    Fetch return fix

--- api/users/controller.js
+++ api/users/controller.js
@@ -1,17 +1,21 @@
-
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 const users = db.get('users')
-const {generateToken, decodeToken} = require('../../token')
+const {
+  generateToken,
+  decodeToken
+} = require('../../token')
 
-db.defaults({users: []}).write()
+db.defaults({
+  users: []
+}).write()
 
 exports.login = async (req, res) => {
   const user = users.find(u => u.email === req.body.email).value()
   const isAvailable = user && user.pwd == req.body.pwd
-  if(isAvailable) {
+  if (isAvailable) {
     const tokenData = {
       id: user.id,
       name: user.name,
@@ -25,8 +29,12 @@ exports.login = async (req, res) => {
       user: user
     })
   } else {
-    user || res.status(500).send({msg: "존재하지 않는 이메일 입니다."})
-    user && res.status(500).send({msg: "비밀번호가 틀립니다."})
+    user || res.status(500).send({
+      msg: "존재하지 않는 이메일 입니다."
+    })
+    user && res.status(500).send({
+      msg: "비밀번호가 틀립니다."
+    })
   }
 };
 
@@ -40,8 +48,10 @@ exports.register = async (req, res) => {
     darkTheme: false
   }
   const isDuplicated = users.find(u => u.email == req.body.email).value()
-  if(isDuplicated){
-    res.status(500).send({msg: "중복된 이메일 입니다."})
+  if (isDuplicated) {
+    res.status(500).send({
+      msg: "중복된 이메일 입니다."
+    })
   } else {
     const tokenData = {
       id: db.get("newUserId").value(),
@@ -62,15 +72,15 @@ exports.register = async (req, res) => {
 
 exports.logout = (req, res) => {
   console.log('logout')
-  res.cookie('user', "", { httpOnly: true, maxAge: 0 })
+  // res.cookie('user', "", { httpOnly: true, maxAge: 0 })
   return res.json('')
 }
 
 exports.fetch = async (req, res) => {
   const token = req.body.tkn
   console.log("Check token", token)
-  if(token){
-    const tokenData = await decodeToken(token)
+  const tokenData = token && await decodeToken(token) || null
+  if (tokenData) {
     const user = await users.find(u => u.id == tokenData.id).value()
     return res.json({
       id: tokenData.id,
@@ -80,11 +90,17 @@ exports.fetch = async (req, res) => {
       darkTheme: user.darkTheme,
       profilePhoto: user.profilePhoto
     })
+  } else {
+    return res.status(500).send({
+      msg: "토큰이 만료되었습니다. 다시 로그인 해주세요."
+    })
   }
 }
 
 exports.theme = async (req, res) => {
   const user = await users.find(u => u.id == req.params.id)
-  await user.assign({darkTheme: !user.value().darkTheme}).write()
+  await user.assign({
+    darkTheme: !user.value().darkTheme
+  }).write()
   return res.json()
 }
